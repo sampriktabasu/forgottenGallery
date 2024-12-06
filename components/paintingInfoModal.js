c617e93a39165c44bb4e5da5b95da90b8a1e5bcb
sampriktabasu <89181348+sampriktabasu@users.noreply.github.com>
2024-12-06 07:36:32 -0800

    initial modal styling

--- components/paintingInfoModal.js
+++ components/paintingInfoModal.js
@@ -3,13 +3,12 @@ export function displayPaintingInfo(info) {
     const modalElement = document.getElementById('modal');
 
     modalElement.innerHTML = `
-    <div class="modal" id="modal">
         <button class="modal-close" id="closeModal">&times;</button>
         <div class="modal-header" id="paintingTitle">${info.title}</div>
         <div><strong>Artist:</strong> <span id="paintingArtist">${info.artist}</span></div>
         <div><strong>Description:</strong> <span id="paintingDescription">${info.description}</span></div>
         <div><strong>Year:</strong> <span id="paintingYear">${info.year}</span></div>
-    </div>`
+    `
 
     modalElement.style.display = 'block';
     modalOverlay.style.display = 'block';
