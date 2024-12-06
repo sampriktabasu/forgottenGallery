69d85c59236484701f5139294130f882aa174986
Aki Hasegawa-Johnson <aki0501@g.ucla.edu>
2024-12-06 14:32:07 -0800

    Clean some code

--- components/controls.js
+++ components/controls.js
@@ -1,8 +1,11 @@
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from 'three';
 
 // Set up camera controls (OrbitControls + custom key handling)
 export function setupControls(camera, domElement) {
   const controls = new OrbitControls(camera, domElement);
+
+  // let blendingFactor = 0.1;
 
   // Custom key movement controls
   function onKeyDown(event) {
@@ -10,17 +13,16 @@ export function setupControls(camera, domElement) {
 
     if (keycode === 39 || keycode === 68) { // Right arrow key or 'D'
       camera.translateX(1);
-      // controls.target.copy(camera.position);
     } else if (keycode === 37 || keycode === 65) { // Left arrow key or 'A'
       camera.translateX(-1);
-      // controls.target.copy(camera.position);
     } else if (keycode === 38 || keycode === 87) { // Up arrow key or 'W'
       camera.translateZ(-1);
-      // controls.target.copy(camera.position);
     } else if (keycode === 40 || keycode === 83) { // Down arrow key or 'S'
       camera.translateZ(1);
-      // controls.target.copy(camera.position);
     }
+
+    // Use lerp to smoothly move the camera towards the target position
+    // camera.position.lerp(targetPosition, blendingFactor);
   }
 
   // Attach keydown event listener
